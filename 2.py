3599764f25c88444f915d48bec6912eb490206f0
samanatuka <60912766+samanatuka@users.noreply.github.com>
2020-02-10 22:35:41 -0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+#inputs the number
+num1 = int(input("Enter the number"))
+num2 = int(input("Enter the second number"))
+#operations on numbers
+add = float(num1) + float(num2)
+sub = float(num1) - float(num2)
+mul = float(num1) * float(num2)
+div = num1/num2
+#printing the numbers
+print(" the sum is {0} difference is {1} multiplication is {2} division is {3}".format(add,sub,mul,div))
