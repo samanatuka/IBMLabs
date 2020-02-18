8783b130cdf4e586acf146a2ae94618511c463d9
samanatuka <60912766+samanatuka@users.noreply.github.com>
2020-02-18 11:45:32 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter number 1"))
+num2=float(input("Enter number 2"))
+num3=float(input("Enter number 3"))
+if(num1 >= num2) and (num1>= num3):
+    largest=num1
+elif(num2 >= num1) and (num2>=num3):
+    largest=num2
+else:
+    largest=num3
+print("the largest number is",largest)
