19a88b3d626c68645d426134222e95742fb9236a
samanatuka <60912766+samanatuka@users.noreply.github.com>
2020-02-25 11:52:54 +0530

    Add files via upload

---
+++ hcf.py
@@ -0,0 +1,13 @@
+def comput_hcf(x,y):
+    if x>y:
+        smaller=y
+    else:
+        smaller=x
+    for i in range(1,smaller+1):
+        if(x % i == 0) and (y % i ==0):
+            hcf = i
+    return hcf
+
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+print("the hcf is", comput_hcf(num1,num2))
