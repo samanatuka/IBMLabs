f2d4d6cfb4c33088837906f920a020cd3ac2318b
samanatuka <60912766+samanatuka@users.noreply.github.com>
2020-02-25 11:03:15 +0530

    Add files via upload

---
+++ literals example.py
@@ -0,0 +1,13 @@
+fruits = ["apple", "mango", "orange"] #list
+numbers = (1, 2, 3) #tuple
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'} #dictionary
+vowels = {'a', 'e', 'i' , 'o', 'u'} #set
+
+for i in fruits:
+    print(i);
+
+
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
