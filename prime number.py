e9079f7e8d3ba54f53fa9c6806aa5872625f0ad3
samanatuka <60912766+samanatuka@users.noreply.github.com>
2020-02-25 11:29:51 +0530

    Add files via upload

---
+++ prime number.py
@@ -0,0 +1,12 @@
+num=int(input("Enter the number"))
+if num >1:
+    for i in range(2,num):
+        if (num%i)==0:
+            print(num,"is not prime number")
+            break
+        else:
+            print(num," is a prime number")
+            break
+else:
+    print("enter a larger number")
+    
